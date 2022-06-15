c195369aac28f7c143840b3040a92c4201df5c35
82106 <littleblair2001@naver.com>
2022-06-15 12:34:27 +0900

    기말고사 과제

--- app/src/main/java/com/example/a11st_app/MovieAdapter.java
+++ app/src/main/java/com/example/a11st_app/MovieAdapter.java
@@ -33,15 +33,28 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     static class ViewHolder extends RecyclerView.ViewHolder{
         TextView movieName;
         TextView peopleCount;
+        TextView sum;
+        TextView ranks;
+        TextView total;
         public ViewHolder(View itemView){
             super(itemView);
             movieName = itemView.findViewById(R.id.movieName);
             peopleCount = itemView.findViewById(R.id.peopleCount);
+            sum = itemView.findViewById(R.id.sum);
+            ranks = itemView.findViewById(R.id.ranks);
+            total = itemView.findViewById(R.id.total);
         }
         public void setItem(Movie item)
         {
             movieName.setText(item.movieNm);
             peopleCount.setText(item.audiCnt);
+            peopleCount.append("명");
+            sum.setText(item.audiAcc);
+            sum.append("명");
+            ranks.setText(item.rank);
+            ranks.append("위");
+            total.setText(item.salesAmt);
+            total.append("원");
         }
     }
 }
